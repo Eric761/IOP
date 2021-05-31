75283e9e77608af136c5c64b51a1c3a738f6d8d5
Fardeen <fkamal@ee.iitr.ac.in>
2021-05-31 11:37:54 +0530

    React App Initialized
    Signed-off-by: Fardeen <fkamal@ee.iitr.ac.in>

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{textAlign: "center"}}>
+      GUI Based Tool for evaluating reliability of distribution system
+    </div>
+  )
+}
+
+export default App;
